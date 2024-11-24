a6cc05b5ff739e9c9e12affa209be750e24d6591
Sujoy Paul <freelancersujoy@gmail.com>
2024-11-24 19:32:38 +0600

    main name

--- src/app.js
+++ src/app.js
@@ -3,12 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    // origin: 'https://codequeryfire.vercel.app/',
-    credentials: true,
-    sameSite: false
-}))
+app.use(cors())
 
 app.use(cookieParser())
 
