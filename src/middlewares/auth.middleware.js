241feabbe7d4feedb7d66d803ee7fd1fad144331
Sujoy Paul <freelancersujoy@gmail.com>
2024-11-24 19:57:00 +0600

    ferer

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { User } from "../models/users.models.js";
 import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/AsyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken'
 
 export const verifyJWT = asyncHandler ( async ( req, res, next ) => {
